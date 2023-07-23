957b58169ce58903dfe128ab4b2db2faf649d268
Gjjj74833 <yihanl21@vt.edu>
2023-07-23 13:28:59 -0400

    update

--- Betti.py
+++ Betti.py
@@ -11,11 +11,13 @@ import matplotlib.pyplot as plt
 import os
 import bisect
 import time
+import random
+from multiprocessing import Pool
 
 
 # The model
 
-def process_rotor_performance(input_file = "Cp_Ct_Cq.NREL5MW.txt"):
+def process_rotor_performance(input_file = "Cp_Ct.NREL5MW.txt"):
     """
     This function will read the power coefficient surface from a text file generated
     by simulation and store the power coefficient in a 2D list
@@ -156,7 +158,7 @@ def drvCpCtCq(omega_R, v_in, beta):
         data = lines[8]
         data_list = data.split()
     
-    return float(data_list[4], data_list[6], data_list[5])
+    return float(data_list[4]), float(data_list[6])
 
 
 def genWind(v_w, end_time, time_step):
@@ -178,6 +180,13 @@ def genWind(v_w, end_time, time_step):
         A list of horizontal wind speed computed at each step
 
     """
+    if end_time < 600:
+        end_time = 600
+        
+    # Generate seeds for random wind model
+    seed1 = random.randint(-2147483648, 2147483647)
+    seed2 = random.randint(-2147483648, 2147483647)
+    seed = [seed1, seed2]
     
     # Replace the path to the Turbsim exe and inp file based on your file location
     path_exe = "C:/Users/ghhh7/Turbsim/TurbSim.exe"
@@ -186,6 +195,12 @@ def genWind(v_w, end_time, time_step):
     # Open the inp file and overwrite with given parameters
     with open(path_inp, 'r') as file:
         lines = file.readlines()
+        
+    # Overwrite with new seeds
+    for i in [3, 4]:
+        line = lines[i].split()
+        line[0] = str(seed[i - 3])
+        lines[i] = ' '.join(line) + '\n'
     
     # Overwrite "AnalysisTime" and "UsableTime"
     for i in [20, 21]:
@@ -226,10 +241,66 @@ def genWind(v_w, end_time, time_step):
         horSpd.append(float(columns[1]))  
 
     return horSpd
+
+def pierson_moskowitz_spectrum(U10, zeta, eta, t, random_phases):
+    """
+    This function generates the Pierson-Moskowitz spectrum for a given wind speed U10 and frequency f.
+    """
+    g = 9.81  # gravitational constant
+    alpha = 0.0081  # Phillips' constant
+
+    f_pm = 0.14*(g/U10)  # peak frequency
     
+    N = 400
+    
+    cutof_f = 3*f_pm # Cutoff frequency
+    
+    f = np.linspace(0.1, cutof_f, N) # Array
+    omega = 2*np.pi*f # Array
+    delta_f = f[1] - f[0] # Array
+
+    S_pm = (alpha*g**2/((2*np.pi)**4*f**5))*np.exp(-(5/4)*(f_pm/f)**4) # Array
+    
+    a = np.sqrt(2*S_pm*delta_f)
+    k = omega**2/g    
+    
+    # Generate random phases all at once
+    
+    
+    # Perform the calculations in a vectorized manner
+    sin_component = np.sin(omega*t - k*zeta + random_phases)
+    cos_component = np.cos(omega*t - k*zeta + random_phases)
+    exp_component = np.exp(k*eta)
+    
+    wave_eta = np.sum(a * sin_component)
+    
+    v_x = np.sum(omega * a * exp_component * sin_component)
+    v_y = np.sum(omega * a * exp_component * cos_component)
+    
+    a_x = np.sum((omega**2) * a * exp_component * cos_component)
+    a_y = -np.sum((omega**2) * a * exp_component * sin_component)
+
+    return wave_eta, [v_x, v_y, a_x, a_y]
+    '''
+    wave_eta = 0
+    v_x = 0
+    v_y = 0
+    a_x = 0
+    a_y = 0
+    
+    for i in range(N):
+        random_phase = 2*np.pi*random.random()
+        wave_eta += a[i]*np.sin(omega[i]*t - k[i]*zeta + random_phase)
+        v_x = omega[i]*a[i]*np.exp(k[i]*eta)*np.sin(omega[i]*t - k[i]*zeta + random_phase)
+        v_y = omega[i]*a[i]*np.exp(k[i]*eta)*np.cos(omega[i]*t - k[i]*zeta + random_phase)
+        a_x = omega[i]**2*a[i]*np.exp(k[i]*eta)*np.cos(omega[i]*t - k[i]*zeta + random_phase)
+        a_y = -omega[i]**2*a[i]*np.exp(k[i]*eta)*np.sin(omega[i]*t - k[i]*zeta + random_phase)
+
+    return wave_eta, [v_x, v_y, a_x, a_y]
+    '''
 
 
-def structure(x_1, beta, omega_R, t, Cp_type, performance, v_w):
+def structure(x_1, beta, omega_R, t, Cp_type, performance, v_w, v_aveg, random_phases):
     """
     The structure of the Betti model
 
@@ -268,16 +339,7 @@ def structure(x_1, beta, omega_R, t, Cp_type, performance, v_w):
     eta = x_1[2] # heave (y) position
     v_eta = x_1[3] # heave velocity
     alpha = x_1[4] # pitch position
-    omega = x_1[5] # pitch velocity
-    
-    v_x = [0, 0]
-    v_y = [0, 0]
-    a_x = [0, 0]
-    a_y = [0, 0]
-    
-
-    
-    
+    omega = x_1[5] # pitch velocity    
     
     g = 9.80665  # (m/s^2) gravity acceleration
     rho_w = 1025  # (kg/m^3) water density
@@ -336,10 +398,12 @@ def structure(x_1, beta, omega_R, t, Cp_type, performance, v_w):
     C_dN = 1 # (-) Nacelle drag coefficient
     A_N = 9.62 # (m^2) Nacelle area
     C_dT = 1 # (-) tower drag coefficient
+    '''
     H_delta = np.array([[-2613.44, 810.13],
                         [810.13, 1744.28]]) # (-) Coefficient for computing deltaFA
     F_delta = np.array([-22790.37, -279533.43]) # (-) Coefficient for computing deltaFA
     C_delta = 10207305.54 # (-) Coefficient for computing deltaFA
+    '''
     A = 12469 # (m^2) Rotor area
     n_dg= 2 # （-） Number of floater sub-cylinders
     C_dgper = 1 # (-) Perpendicular cylinder drag coefficient
@@ -460,8 +524,23 @@ def structure(x_1, beta, omega_R, t, Cp_type, performance, v_w):
     Qh_alpha = 0
     Qwa_alpha = 0
     
+    v_x = [0, 0]
+    v_y = [0, 0]
+    a_x = [0, 0]
+    a_y = [0, 0]
+    height = [0, 0]
+    
     for i in range(n_dg):
+        
         h_pg[i] = (i + 1 - 0.5)*h_sub/n_dg
+        height[i] = -(h_sub - h_pg[i])
+        
+        wave = pierson_moskowitz_spectrum(v_aveg, zeta, height[i], t, random_phases)[1]
+        
+        v_x[i] = wave[0]
+        v_y[i] = wave[1]
+        a_x[i] = wave[2]
+        a_y[i] = wave[3]
         
         v_per[i] =  ((v_zeta + (h_pg[i] - d_Sbott)*omega*np.cos(alpha) - v_x[i])*np.cos(alpha)
                      + (v_eta + (h_pg[i] - d_Sbott)*omega*np.sin(alpha) - v_y[i])*np.sin(alpha))
@@ -526,20 +605,37 @@ def WindTurbine(omega_R, v_in, beta, T_E, t, Cp, Cp_type):
     
     return domega_R
 
+def genWind_parallel():
+    """
+    This function generate wind for n simulations before fun the model.
+    It will store the wind generated for each simulation in a 2D list.
+    Everytime this function get called, it will return on wind in vWind_parallel
+    and remove that wind from vWind_parallel.
 
-#def wave(t):
-    '''
-    def S_PM(f):
-        
-        alpha_PM = 0.0081
-        g = 9.80665  # (m/s^2) gravity acceleration
-        
-        S_PM = ((alpha_PM*g**2)/((2*np.pi)**4*f**5))*np.e**(-1.25*(f/f_PM)**(-4))
-    '''
+    Parameters
+    ----------
+    v_w : TYPE
+        DESCRIPTION.
+    end_time : TYPE
+        DESCRIPTION.
+    time_step : TYPE
+        DESCRIPTION.
+    n_simulations : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    wind = vWind_parallel[-1]
+    vWind_parallel.pop()
+    return wind
     
     
+    
 
-def Betti(x, t, beta, T_E, Cp_type, performance, v_w):
+def Betti(x, t, beta, T_E, Cp_type, performance, v_w, v_aveg, random_phases):
     """
     Combine the WindTurbine model and structure model
     
@@ -567,7 +663,7 @@ def Betti(x, t, beta, T_E, Cp_type, performance, v_w):
     x1 = x[:6]
     omega_R = x[6]
     
-    dx1dt, v_in, Cp = structure(x1, beta, omega_R, t, Cp_type, performance, v_w)
+    dx1dt, v_in, Cp = structure(x1, beta, omega_R, t, Cp_type, performance, v_w, v_aveg, random_phases)
     dx2dt = WindTurbine(omega_R, v_in, beta, T_E, t, Cp, Cp_type)
     dxdt = np.append(dx1dt, dx2dt)
     
@@ -611,14 +707,17 @@ def rk4(Betti, x0, t0, tf, dt, beta, T_E, Cp_type, performance, v_w):
     x[0] = x0
     
     v_wind = genWind(v_w, tf, dt)
+    #v_wind = genWind_parallel()
+    
+    random_phases = 2 * np.pi * np.random.rand(400)
 
-    #count = 0
+    count = 0
 
     for i in range(n - 1):
-        k1 = Betti(x[i], t[i], beta, T_E, Cp_type, performance, v_wind[i])
-        k2 = Betti(x[i] + 0.5 * dt * k1, t[i] + 0.5 * dt, beta, T_E, Cp_type, performance, v_wind[i])
-        k3 = Betti(x[i] + 0.5 * dt * k2, t[i] + 0.5 * dt, beta, T_E, Cp_type, performance, v_wind[i])
-        k4 = Betti(x[i] + dt * k3, t[i] + dt, beta, T_E, Cp_type, performance, v_wind[i])
+        k1 = Betti(x[i], t[i], beta, T_E, Cp_type, performance, v_wind[i], v_w, random_phases)
+        k2 = Betti(x[i] + 0.5 * dt * k1, t[i] + 0.5 * dt, beta, T_E, Cp_type, performance, v_wind[i], v_w, random_phases)
+        k3 = Betti(x[i] + 0.5 * dt * k2, t[i] + 0.5 * dt, beta, T_E, Cp_type, performance, v_wind[i], v_w, random_phases)
+        k4 = Betti(x[i] + dt * k3, t[i] + dt, beta, T_E, Cp_type, performance, v_wind[i], v_w, random_phases)
         x[i + 1] = x[i] + dt * (k1 + 2*k2 + 2*k3 + k4) / 6
         
         # Convert pitch anlge, velocity to deg and deg/s, rotor speed to rpm
@@ -626,16 +725,23 @@ def rk4(Betti, x0, t0, tf, dt, beta, T_E, Cp_type, performance, v_w):
         x[i][5] = np.rad2deg(x[i][5])
         x[i][6] = (60 / (2*np.pi)) * x[i][6]
         x[i][2] = x[i][2] - 40.612
-        if i == n - 1:
-            x[i + 1][4] = np.rad2deg(x[i + 1][4])
-            x[i + 1][5] = np.rad2deg(x[i + 1][5])
-            x[i + 1][6] = (60 / (2*np.pi)) * x[i + 1][6]
-            x[i + 1][2] = x[i + 1][2] - 40.612
-
-        #count += 1
+        x[i][0] = - x[i][0]
+        
+        count += 1
         #print(count)
 
-    return t, x, v_wind[:len(t)]
+    x[-1][4] = np.rad2deg(x[-1][4])
+    x[-1][5] = np.rad2deg(x[-1][5])
+    x[-1][6] = (60 / (2*np.pi)) * x[-1][6]
+    x[-1][2] = x[-1][2] - 40.612
+    x[-1][0] = - x[-1][0]            
+
+    # Output wave elevation at zeta = 0
+    wave_eta = []
+    for i in t:
+        wave_eta.append(pierson_moskowitz_spectrum(v_w, 0, 0, i, random_phases)[0])
+
+    return t, x, v_wind[:len(t)], wave_eta
 
 
 
@@ -652,11 +758,20 @@ def main(end_time, v_w, time_step = 0.01, Cp_type = 0):
 
     Returns
     -------
-    None.
-
+    t: np.array
+        The time array
+    x: 2D array:
+        The state at each time.The row of x corresponding to each time step.
+        The column is each state [surge, surge_velocity, heave, heave_velocity, pitch, pitch_rate, rotor_speed]
+    v_wind: list
+        The wind speed at each time step
+    wave_eta: list
+        The wave elevation at surge = 0 for each time step
     """
+    '''
     CPU_start = time.process_time()
     start = time.time()
+    '''
     
     performance = process_rotor_performance()
     
@@ -664,22 +779,39 @@ def main(end_time, v_w, time_step = 0.01, Cp_type = 0):
     
     # modify this to change initial condition
     #[zeta, v_zeta, eta, v_eta, alpha, omega, omega_R]
-    x0 = np.array([0, 0, 40.612, 0, 0, 0, 1])
+    x0 = np.array([0, 0, 40.612 - 3, 0, 0, 0, 1])
 
     # modify this to change run time and step size
     #[Betti, x0 (initial condition), start time, end time, time step, beta, T_E]
-    t, x, v_wind = rk4(Betti, x0, start_time, end_time, time_step, 0.4, 43000, Cp_type, performance, v_w)
-
+    t, x, v_wind, wave_eta = rk4(Betti, x0, start_time, end_time, time_step, 0.4, 43000, Cp_type, performance, v_w)
+    
+    # return the output to be ploted
+    return t, x, v_wind, wave_eta
+    '''
     state_names = ['Surge (m)', 'Surge Velocity (m/s)', 'Heave (m)', 'Heave Velocity (m/s)', 
                    'Pitch Angle (deg)', 'Pitch Rate (deg/s)', 'Rotor speed (rpm)']
-
-    plt.figure()  # create a new figure for wind velocity
+    
+    
+    # Plotting the wind velocity
+    plt.figure() 
     plt.plot(t, v_wind)
     plt.xlabel('Time')
     plt.ylabel('Wind Speed')
     plt.title('Time evolution of Wind Speed')
     plt.grid(True)
-    plt.xlim(0, end_time)
+    plt.xlim(end_time -1000, end_time)
+    plt.savefig('wind_speed.png', dpi=600)  
+    plt.show()
+    
+    # Plotting the sea surface elevation
+    plt.figure()
+    plt.plot(t, wave_eta)
+    plt.title('Time evolution of Sea Surface Elevation at position x')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Elevation (m)')
+    plt.grid(True)
+    plt.xlim(end_time - 1000, end_time)
+    plt.savefig('wave.png', dpi=600) 
     plt.show()
     
     for i in range(x.shape[1]):
@@ -689,7 +821,9 @@ def main(end_time, v_w, time_step = 0.01, Cp_type = 0):
         plt.ylabel(f'{state_names[i]}')
         plt.title(f'Time evolution of {state_names[i]}')
         plt.grid(True)
-        plt.xlim(0, end_time)
+        plt.xlim(end_time - 1000, end_time)
+        safe_filename = state_names[i].replace('/', '_')  
+        plt.savefig(f'{safe_filename}.png', dpi=600)  
         plt.show()
         
     CPU_end = time.process_time()
@@ -697,21 +831,98 @@ def main(end_time, v_w, time_step = 0.01, Cp_type = 0):
     
     print("CPU time: ", CPU_end - CPU_start, "seconds")
     print("time: ", end - start, "seconds")
+    '''
+
+
+def run_simulation(params):
+    return main(*params)
+
+
+def run_simulations_parallel(n_simulations, params):
         
-        
+    with Pool() as p:
+        results = p.map(run_simulation, [params]*n_simulations)
+    
+    end_time = params[0]
+
+    state_names = ['Surge (m)', 'Surge Velocity (m/s)', 'Heave (m)', 'Heave Velocity (m/s)', 
+                   'Pitch Angle (deg)', 'Pitch Rate (deg/s)', 'Rotor speed (rpm)']
+
+    color = (0, 0, 1, 0.5)
+
+    
+    # Plotting the wind velocity
+    plt.figure() 
+    for i, result in enumerate(results):
+        t, _, v_wind, _ = result
+        plt.plot(t, v_wind, color=color)
+    plt.xlabel('Time')
+    plt.ylabel('Wind Speed')
+    plt.title('Time evolution of Wind Speed')
+    plt.grid(True)
+    plt.xlim(0, end_time)
+    plt.savefig('wind_speed.png', dpi=2000)  
+    plt.show()
+
+    
+    # Plotting the sea surface elevation
+    plt.figure()
+    for i, result in enumerate(results):
+        t, _, _, wave_eta = result
+        plt.plot(t, wave_eta, color=color)
+    plt.title('Time evolution of Sea Surface Elevation at position x')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Elevation (m)')
+    plt.grid(True)
+    plt.xlim(0, end_time)
+    plt.savefig('wave.png', dpi=2000) 
+    plt.show()
+
+    
+    for state_index in range(len(state_names)):
+        plt.figure()  # create a new figure for each state
+        for i, result in enumerate(results):
+            t, x, _, _ = result
+            plt.plot(t, x[:, state_index], color=color)
+        plt.xlabel('Time')
+        plt.ylabel(f'{state_names[state_index]}')
+        plt.title(f'Time evolution of {state_names[state_index]}')
+        plt.grid(True)
+        plt.xlim(0, end_time)
+        safe_filename = state_names[state_index].replace('/', '_')  
+        plt.savefig(f'{safe_filename}.png', dpi=2000)  
+        plt.show()
+    
+    #return results
+
+
+
+
+
+########################################
 ###############################################################################
-###############################################################################
         
 
 
+if __name__ == '__main__':
+    params = [10, 20]
+    n_simulations = 3
+    '''
+    vWind_parallel = []
+    for i in range(n_simulations):
+        v_w = genWind(params[1], params[0], 0.01)
+        vWind_parallel.append(v_w)
+    '''
+  
+    
+    run_simulations_parallel(n_simulations, params)
 
-main(1000, 20)
-
-
+    
 '''
+
 performance = process_rotor_performance()
-print(performance[1])
-#print(C_p(6.5, 0, performance), Cp(1.2566, 12, 0))
+
+print(CpCtCq(-10.08, -5, performance), drvCpCtCq(1.2566, 12, 0))
 
 '''
 
--- writeCp.py
+++ writeCp.py
@@ -11,57 +11,25 @@ import matplotlib.pyplot as plt
 from Betti import drvCpCtCq
 from Betti import process_rotor_performance
 
-def write(pitch_step = 0.1, omega_step = 0.01, v_w = 10):
-    
-    with open('Cp_Ct_Cq.NREL5MWneg.txt', 'w') as file:
-        
-        R = 63 # (m) Radius of rotor 
-        
-        # Create a list including all pitch angles to compute
-        pitch_angle = np.arange(-5, 45.1, pitch_step).tolist()
-        pitch_str = ' '.join("{:.2f}".format(num) for num in pitch_angle)
-        
-        # Create a list including all rotor speed to compute
-        rotor_speed = np.arange(-1.6, 3, omega_step).tolist()
-        TSR = []
-        for i in rotor_speed:
-            TSR.append((i*R)/v_w)
-        TSR_str = ' '.join("{:.6f}".format(num) for num in TSR)
-        
-        # write header
-        file.write("# ----- Rotor performance tables for the NREL-5MW wind turbine ----- \n")
-        file.write("# ------------ Written on Jan-13-22 using the ROSCO toolbox ------------ \n")
-        file.write(" \n")
-        file.write("# Pitch angle vector, 36 entries - x axis (matrix columns) (deg) \n")
-        file.write(pitch_str + "\n")
-        file.write("# TSR vector, 26 entries - y axis (matrix rows) (-) \n")
-        file.write(TSR_str + "\n")
-        file.write("# Wind speed vector - z axis (m/s) \n")
-        file.write(str(v_w) + "\n")
-        file.write(" \n")
-        file.write("# Power coefficient \n")
-        file.write(" \n")
-        
-        # Run the AeroDyn v15 driver and write the power coefficient by row
-        count = [0, 0]
-        # For each row (rotor speed value)
-        for i in rotor_speed:
-            line = []
-            # For each column (pitch angle value)
-            for j in pitch_angle:
-                Cp = drvCpCtCq(i, v_w, np.deg2rad(j))[0]
-                line.append(Cp)
-                count[1] += 1
-                print(count)
-            # Write the line of data
-            line_str = ' '.join("{:.6f}".format(num) for num in line)
-            file.write(line_str + "\n")
-            
-            count[0] += 1
-            count[1] = 0
-            
             
 def write(pitch_step = 0.1, omega_step = 0.01, v_w = 10):
+    """
+    Write the power coefficient and thrust coefficient file
+
+    Parameters
+    ----------
+    pitch_step : float, optional
+        The step size of pitch angle. The default is 0.1.
+    omega_step : float, optional
+        The step size of the rotor speed. The default is 0.01.
+    v_w : float, optional
+        The constant wind velocity. The default is 10.
+
+    Returns
+    -------
+    None.
+
+    """
     
     with open('Cp_Ct_Cq.NREL5MWneg.txt', 'w') as file:
         
@@ -97,11 +65,11 @@ def write(pitch_step = 0.1, omega_step = 0.01, v_w = 10):
         # For each row (rotor speed value)
         Cp_str = ""
         Ct_str = ""
-        Cq_str = ""
+       
         for i in rotor_speed:
             lineCp = []
             lineCt = []
-            lineCq = []
+            
             # For each column (pitch angle value)
             for j in pitch_angle:
                 Cp, Ct = drvCpCtCq(i, v_w, np.deg2rad(j))
@@ -133,6 +101,10 @@ def write(pitch_step = 0.1, omega_step = 0.01, v_w = 10):
 
             
 def visualCp():
+    """
+    Plot the power coefficient surface
+
+    """
     
     performance = process_rotor_performance()
         
